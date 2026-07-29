6162d9a92059f0f3497e674bbbe8353d970ee060
Leo <im.yczeng@foxmail.com>
2026-07-29 21:43:09 +0800

    emacs: add early-init.el

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- Early startup settings -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Code:
+(setq gc-cons-threshold most-positive-fixnum)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;;; early-init.el ends here
